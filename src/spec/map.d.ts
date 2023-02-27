69fc3598ba0104651b10ba1be108ff43d4e8a46d
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2023-02-27 10:44:15 +0700

    Handle map

---
+++ src/spec/map.d.ts
@@ -0,0 +1,72 @@
+import L from "leaflet";
+interface MapConfigs {
+  id: string | HTMLElement;
+  center: L.LatLngExpression;
+  zoom?: number;
+  worldCopyJump?: boolean;
+  minZoom?: number;
+  maxZoom?: number;
+  clusteredNumber?: number;
+  clusteredDisableAtZoom?: number;
+  options?: L.MapOptions;
+}
+interface FeatureGroup {
+  name: string;
+  layer: L.FeatureGroup;
+}
+interface Marker {
+  name: string;
+  layer: L.Marker;
+}
+interface FeatureGroupInput {
+  name: string;
+}
+interface MarkerInput {
+  latlng: L.LatLngExpression;
+  name: string;
+  options?: L.MarkerOptions;
+}
+interface TileLayerInput {
+  urlTemplate: string;
+  options?: L.TileLayerOptions;
+}
+interface LatLng {
+  lat: number;
+  lng: number;
+}
+interface LatLngBounds {
+  southWest: L.LatLngExpression;
+  northEast: L.LatLngExpression;
+}
+interface CustomLatLngBounds {
+  bounds: L.LatLngBounds;
+  markers: Marker[];
+}
+interface GeoPicture {
+  name: string;
+  layer: L.Path;
+}
+interface PolylineInput {
+  name: string;
+  latlngs: L.LatLngExpression[];
+  options?: L.PolylineOptions;
+}
+interface PolygonInput {
+  name: string;
+  latlngs: L.LatLngExpression[];
+  options?: L.PolylineOptions;
+}
+interface RectangleInput {
+  name: string;
+  latlngBounds: L.LatLngBounds;
+  options?: L.PolylineOptions;
+}
+interface CircleInput {
+  name: string;
+  latlng: L.LatLngExpression;
+  radius: number;
+  options?: CustomCircleOptions;
+}
+interface CustomCircleOptions extends L.CircleOptions {
+  radius?: number;
+}
