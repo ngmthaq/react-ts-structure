758895a582ac74732be757947ee593a74afe7e38
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2023-03-25 21:48:56 +0700

    Update components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { FC, Fragment } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "plugins/router";
+import Loading from "components/Loading";
+import Notifications from "components/Notifications";
 
-const App: React.FC<{}> = () => {
-  return <RouterProvider router={router} />;
+const App: FC<{}> = () => {
+  return (
+    <Fragment>
+      <RouterProvider router={router} />
+      <Loading />
+      <Notifications />
+    </Fragment>
+  );
 };
 
 export default App;
---
+++ src/components/Notifications/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Notifications: FC<{}> = () => {
+  return <div></div>;
+};
+
+export default Notifications;
