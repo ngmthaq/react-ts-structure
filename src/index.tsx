32f7faa3078994ae3c35b9ffbb6809592aff2fa1
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2023-03-25 11:55:07 +0700

    Refactor directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+const Component: React.FC<{}> = () => {
+  return <App />;
+};
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  process.env.NODE_ENV === "production" ? (
+    <React.StrictMode>
+      <Component />
+    </React.StrictMode>
+  ) : (
+    <Component />
+  ),
 );
 
 // If you want your app to work offline and load faster, you can change
@@ -22,4 +28,4 @@ serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(process.env.NODE_ENV !== "production" ? console.table : () => {});
