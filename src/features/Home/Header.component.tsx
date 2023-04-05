525ac9203e0ec316bcfec4dd96fdda32c1c4d940
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2023-04-05 17:35:17 +0700

    Remove code

--- src/features/Home/Header.component.tsx
+++ src/features/Home/Header.component.tsx
@@ -1,28 +1,7 @@
-import React, { useEffect } from "react";
-import { useAppSearchParams } from "plugins/hooks";
+import React from "react";
 import styles from "./Home.module.scss";
-import EventBus from "plugins/bus";
-import { EventBusType } from ".";
 
 const Header: React.FC<{}> = () => {
-  const { appSearchParams } = useAppSearchParams();
-
-  useEffect(() => {
-    console.info(appSearchParams);
-  }, [appSearchParams]);
-
-  useEffect(() => {
-    EventBus.on<EventBusType>("testEventBus", data => {
-      console.info(data);
-    });
-
-    return () => {
-      EventBus.off<EventBusType>("testEventBus", data => {
-        console.info(data);
-      });
-    };
-  });
-
   return <p className={styles.paragraph}>Header</p>;
 };
 
--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,31 +1,7 @@
 import React from "react";
-import { useAppTranslation, useAppSearchParams } from "plugins/hooks";
-import styles from "./Home.module.scss";
-import Header from "./Header.component";
-import { PATH_HOME } from "const/path.const";
-import EventBus from "plugins/bus";
 
 const Home: React.FC<{}> = () => {
-  const { getLabel } = useAppTranslation();
-  const { appSearchParams, changeAppSearchParams, redirect } = useAppSearchParams();
-
-  return (
-    <div>
-      <Header />
-      <p className={styles.paragraph}>{getLabel("title.Home")}</p>
-      <button onClick={() => changeAppSearchParams({ a: "2" })}>Change</button>
-      <button onClick={() => redirect("/a", appSearchParams)}>Redirect to another page</button>
-      <button onClick={() => redirect(PATH_HOME.path, appSearchParams)}>Redirect to this page</button>
-      <button onClick={() => EventBus.emit<EventBusType>("testEventBus", { hello: "world", event: "bus" })}>
-        Emit
-      </button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
-
-export type EventBusType = {
-  hello: string;
-  event: string;
-};
