8e9fabf7b9169f0f84bc3fc72a56485a183c5034
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2023-04-06 15:09:16 +0700

    Install hook pwa

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { usePWA } from "plugins/pwa";
 
 const Home: React.FC<{}> = () => {
-  return <div></div>;
+  const { registration } = usePWA();
+
+  const onShowNotification = () => {
+    if (registration) {
+      registration.showNotification("Notification title");
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={onShowNotification}>Open notification</button>
+    </div>
+  );
 };
 
 export default Home;
